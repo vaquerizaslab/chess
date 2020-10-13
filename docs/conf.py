3bb135c0135c4a1940efadc8c8f0f1b52a7b87c1
Nick Machnik <nick.machnik@gmail.com>
2020-10-13 11:45:32 +0200

    Add skimage.metrics to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,8 @@ sys.path.insert(0, os.path.abspath('..'))
 import mock
 MOCK_MODULES = [
     'numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
-    'scipy.interpolate', 'intervaltree', 'skimage', 'skimage.transform'
+    'scipy.interpolate', 'intervaltree', 'skimage', 'skimage.transform',
+    'skimage.metrics'
 ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
