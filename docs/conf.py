9dd0b8d736f36a3827dea5cad330d53e3dcf75e6
Nick Machnik <nick.machnik@gmail.com>
2020-10-13 11:58:06 +0200

    Add skimage.morphology to mock

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,8 @@ import mock
 MOCK_MODULES = [
     'numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
     'scipy.interpolate', 'intervaltree', 'skimage', 'skimage.transform',
-    'skimage.metrics', 'numpy.lib.stride_tricks', 'numpy.lib'
+    'skimage.metrics', 'numpy.lib.stride_tricks', 'numpy.lib',
+    'skimage.morphology'
 ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
