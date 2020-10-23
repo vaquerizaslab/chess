cbb07f9e0964b1628b6edcdb1c20157703688fa6
Nick Machnik <nick.machnik@gmail.com>
2020-10-23 10:21:10 +0200

    Require fanc 0.9.6

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         'cython',
         'numpy>=1.8.0',
-        'fanc>=0.9.5',
+        'fanc>=0.9.6',
         'scikit-image',
         'future',
         'pathos',
